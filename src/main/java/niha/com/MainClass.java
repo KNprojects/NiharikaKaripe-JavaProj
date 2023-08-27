1e577fa6040865c7cbe7906a7695232af026d3b0
KNprojects <karipe.niharikaa@gmail.com>
2023-08-27 13:20:53 +0530

    modified the main class

---
+++ src/main/java/niha/com/MainClass.java
@@ -0,0 +1,11 @@
+package niha.com;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to World!!");
+
+	}
+
+}
